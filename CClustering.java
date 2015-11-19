75ed3682acb7dd04d0572e79e863647ea9953c03
2-bytes <2_bytes@ukr.net>
2015-11-19 16:06:42 +0200

    Add function to load data from *.xlsx file.

--- CClustering.java
+++ CClustering.java
@@ -5,18 +5,49 @@
  */
 package cclusteringmodified;
 
-
+import cclusteringmodified.utils.ExcelDataLoader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 /**
  *
  * @author Vitalii Umanets
  */
 public class CClustering {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            double[][] data = ExcelDataLoader.loadXLSXFile("I:\\Book1.xlsx", false);
+            StringBuilder spaces = new StringBuilder();
+            int maxsize=0;
+            int cellsize=0;
+            for(int i=0;i<data.length;i++)
+                for(int j=0;j<data[0].length;j++){
+                    cellsize=String.valueOf(Math.round(data[i][j])).length();
+                    maxsize=maxsize<cellsize?cellsize:maxsize;
+            }
+
+            for(int i=0;i<maxsize;i++)
+            spaces.append(' ');
+
+            for(int i=0;i<data.length;i++){
+                for(int j=0;j<data[0].length;j++){
+                    cellsize=String.valueOf(Math.round(data[i][j])).length();
+                    spaces.delete(maxsize, spaces.length());
+                    spaces.insert(maxsize, data[i][j]<0?"":" ");
+                    spaces.append("%.").append(4).append("f ");
+                    System.out.format(spaces.substring(cellsize-1), data[i][j]);
+                }
+                System.out.println();
+            }
+            System.out.println();
+
+        } catch (IOException ex) {
+            Logger.getLogger(CClustering.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InvalidFormatException ex) {
+            Logger.getLogger(CClustering.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
 }
--- utils/ExcelDataLoader.java
+++ utils/ExcelDataLoader.java
@@ -11,13 +11,30 @@ package cclusteringmodified.utils;
 import java.io.File;
 import java.io.IOException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 
 public class ExcelDataLoader {
 
-    static void loadXLSXFile(String path) throws IOException, InvalidFormatException{
+    /**
+     * Loads data from the given file as an array of numeric values
+     * Attention: Data is loaded from the first list.
+     * @param path The path of the *.xlsx file to read.
+     * @param omitFirstRow If the first row has names then this flag should be set to true to ignore it.
+     * @throws IOException
+     * @throws InvalidFormatException
+     * @return
+     */
+    public static double[][] loadXLSXFile(String path, boolean omitFirstRow) throws IOException, InvalidFormatException{
         XSSFWorkbook wb = new XSSFWorkbook(new File(path));
-        wb.getFirstVisibleTab();
+        XSSFSheet sh = wb.getSheetAt(0); // Only the first one
+        int shift = omitFirstRow? 1 : 0;
+        double[][] data = new double[sh.getLastRowNum() + 1 - shift][sh.getRow(shift).getPhysicalNumberOfCells()];
+        for(int i=0;i < data.length;i++)
+            for(int j=0; j < data[0].length;j++){
+                data[i][j] = sh.getRow(i+shift).getCell(j).getNumericCellValue();
+            }
+        return data;
     }
 }
